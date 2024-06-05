9163c90e913481701af9aa47ebfd3012c8b58628
tpgus2603 <kakaneymar2424@gmail.com>
2024-06-05 19:10:16 +0900

    third

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(), // 데이터를 담는 상태를 만듬 
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) { //보여줄때 build함수 씀 
+    var appState = context.watch<MyAppState>(); //watch :변화가 있을때 알려달란 함수 
+
+    return Scaffold(          
+      body: Column(
+        children: [
+          Text('A random idea:'),
+          Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
+        ],
+      ),
+    );
+  }
+}
